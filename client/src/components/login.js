88098a3310738c6302fd1efd9e127989d2240137
KimTaeHyun03 <rlaxogus03@gmail.com>
2025-01-29 22:34:32 +0900

    develop login component

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,9 +1,13 @@
 import './../css/login.css';
 
+import {useState} from 'react';
 import {Link} from 'react-router-dom';
 import { FaUserCircle } from "react-icons/fa";
 
 let Login = ()=>{
+  
+  let [showPw,setShowPw] = useState(false);
+  
   return(
     <div className='loginContainer'>
       <div className='loginfilterContainer'/>
@@ -22,8 +26,26 @@ let Login = ()=>{
         type='text'
         placeholder='아이디를 입력하세요'
         className='loginInput'
-        
         />
+        
+        <input 
+        type={showPw ? 'text' : 'password'}
+        placeholder='비밀번호를 입력하세요'
+        className='loginInput'
+        />
+        <input 
+        type={showPw ? 'text' : 'password'}
+        placeholder='비밀번호를 또 입력하세요'
+        className='loginInput'
+        />
+          <label className='loginPwShowContainer'
+          >
+        <input type='checkbox' onChange={()=>{
+          setShowPw(!showPw);
+        }}></input>    
+            <span>비밀번호 보기</span>
+          </label>
+        
         
       </div>
       
